bd6e9ecac4855b823b87e1b38e6c7c0999403a86
Dixie Cravens <dcravens@gmail.com>
2020-09-17 11:44:41 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ConsoleApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Dixie Cravens";
+            string location = "Knoxville, Tennessee";
+
+            DateTime currentDateTime = DateTime.Now;           
+            // Get date-only portion of date, without its time.
+            DateTime dateOnly = currentDateTime.Date;
+
+            DateTime christmas2020 = Convert.ToDateTime("12/25/2020");
+            double numberOfDays = (christmas2020 - currentDateTime).Days;
+
+            Console.WriteLine("My name is " + name + ", and I am from " + location + ".");
+            Console.WriteLine(dateOnly.ToString("d"));
+            Console.WriteLine(numberOfDays + " days until Christmas 2020!");
+        }
+    }
+}
